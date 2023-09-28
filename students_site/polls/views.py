c38877f409e193871b398f5c28663d203b0453d1
TinkoEd <228rubiks228@gmail.com>
2023-09-28 14:14:36 +0300

    added int error

--- students_site/polls/views.py
+++ students_site/polls/views.py
@@ -15,8 +15,11 @@ def get_student(request):
 
 
 def get_students(request):
-    count = int(request.GET.get('count'))
-    if not isinstance(count, int) or count > 100 or count <= 0:
+    try:
+        count = int(request.GET.get('count', ''))
+    except ValueError:
+        return HttpResponse(f'<h3>Error! Enter count=(a natural number from 1 to 100)</h3>')
+    if count > 100 or count <= 0:
         return HttpResponse(f'<h3>Error! Enter a natural number from 1 to 100</h3>')
     for _ in range(count):
         Students.objects.create(first_name=fake.first_name(), last_name=fake.last_name(), birth_date=fake.date())
